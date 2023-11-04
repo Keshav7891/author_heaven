33af961996fc805fac2527f7181f4d64e6f85c24
Keshav Allawadi <keshavallawadi7891@gmail.com>
2023-11-04 20:30:17 +0530

    adding initial setup

---
+++ authors_api/settings/local.py
@@ -0,0 +1,15 @@
+from .base import * #noqa
+from .base import env
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("DJANG_SECRET_KEY" ,default='d74ToRwfY6z43DcLVuoEssPmVQukmWAmDe9wJIMxh8WYFTecPeY',)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
